ef4f369319a391830d573cbff87a7c8c682b7afc
Maxi Maidana <maidana1454@gmail.com>
2022-07-13 20:12:59 -0300

    se corrige ruta

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:victor_tauro/core/widget_tree.dart';
 
-import '../features/home/presentation/pages/home_page.dart';
+import '../../features/home/presentation/pages/home_page.dart';
 
 Map<String, Widget Function(BuildContext)> mapRoutes() => {
       '/': (BuildContext context) => const WidgetTree(),
